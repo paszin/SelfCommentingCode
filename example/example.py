b57423f1112178e670a07a2a34cb862544937db7
Pascal <pascalslogin@gmail.com>
2016-05-14 19:13:57 -0700

    changed example from ruby to python

---
+++ example/example.py
@@ -0,0 +1,8 @@
+
+import sys
+import json
+import random
+
+a = random.random()
+e = a + 1
+
--- example/example.rb
+++
@@ -1,6 +0,0 @@
-a = 6
-e = a + 1
-
-def getAllRules():
-    return [obj for name, obj in inspect.getmembers(sysrrrrrr.modules[__name__]) if insperrrrrct.isclass(obj)]
- 
