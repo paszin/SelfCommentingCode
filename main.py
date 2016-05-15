449412b54471d4962ec9736480b0c5803b644c54
Pascal <pascalslogin@gmail.com>
2016-05-15 14:20:19 -0700

    added options

--- main.py
+++ main.py
@@ -2,11 +2,29 @@
 ## do this for compatibility with version 3
 from __future__ import print_function
 
+import random
 import os
 import sys
-
+import argparse
 
 import patternFinder
+
+
+
+## parse arguments
+parser = argparse.ArgumentParser(description='this serious tool will make your code more readable, easier to understand and more fun to collaborate')
+parser.add_argument('--path', metavar='P', type=str,
+                   help='path to your codebase')
+parser.add_argument('--skip', action="store_true",
+                   help='whatever')
+parser.add_argument('--savedb', action="store_true",
+                   help='save results to database')
+
+
+#args = parser.parse_args()
+#path = args.path
+#skip = args.skip
+#savedb = args.savedb
 
 try:
 	path = sys.argv[1]
@@ -15,6 +33,9 @@ except:
 
 endings = ["py"]
 commentPrefix = '#&# '
+noFrequency = True
+newFile = True
+onlyOneComment = False
 
 for dirpath, dirnames, filenames in os.walk(path):
 	for fn in filenames:
@@ -28,7 +49,7 @@ for dirpath, dirnames, filenames in os.walk(path):
 				for i, line in enumerate(contentList):
 					for rulec in patternFinder.getAllRules():
 						rule = rulec(content, line.strip())
-						if rule.isMatching():
+						if rule.isMatching() and (random.random() <= rule.frequency or noFrequency):
 							prefix = line.split(line.strip())[0] #code with indentation
 							comment = rule.getComment()
 							offset += 1
@@ -36,6 +57,8 @@ for dirpath, dirnames, filenames in os.walk(path):
 								offset += (len(comment) - 2)
 								comment = ('\n' + prefix + commentPrefix).join(comment)
 							contentListNew.insert(i+offset-1, prefix + commentPrefix + comment)
+							if onlyOneComment:
+								break
 				print('\n'.join(contentListNew))
 			with open(os.path.join(dirpath, fn+".scc"), "w") as f:
 				f.write('\n'.join(contentListNew))
