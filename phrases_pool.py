436cc00275e348d9a5a3b350bd5b3ce7dccf10ac
Pascal <pascalslogin@gmail.com>
2016-05-15 15:54:01 -0700

    improved rules

--- phrases_pool.py
+++ phrases_pool.py
@@ -1,13 +1,19 @@
 import random
+import hackercomments
 
 opinions = ["This is amazing", "Awesome", "Ugly Code", "Fancy Hack"]
 def getRandomOpinion():
 	return random.choice(opinions)
 
-adjectives = ["famous", "popular", "advanced"]
+adjectives = ["famous", "popular", "advanced", "radical", "under-appreciated"]
 def getRandomAdjective():
 	return random.choice(adjectives)
 
-varMeanings = ["stores the state of the process", "is used for seed data", "contains all filtered objects"]
+varMeanings = ["stores the state of the process", "is used for seed data", "contains all filtered objects", "is a semaphores tor thread locking"]
 def getRandomMeaning():
 	return random.choice(varMeanings)
+
+def getRandomHackerComment():
+	return random.choice(hackercomments.data)
+
+
