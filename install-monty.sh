efc918cef00fb1dcc8fdec2d03fcb4f269fd033f
Carlos Isales <cisalesvf@gmail.com>
2016-05-15 14:32:22 -0700

    push up right version of install script

--- install-monty.sh
+++ install-monty.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-"python $1/main.py
+echo "python $1/main.py
 git add .
 exit 0" > $PWD/.git/hooks/pre-commit
 
-"#!/bin/sh
-$1/main.py " + '$@' > /usr/local/bin/monty.sh
+echo "#!/bin/sh
+python $1main.py" '$@' > /usr/local/bin/monty.sh
